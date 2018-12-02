94325959dbbbdc7cfc4e0a2c4197823c5f7147c4
Chulgon <cck1284@naver.com>
2018-12-02 22:51:18 +0900

    test

---
+++ location.php
@@ -0,0 +1,16 @@
+<? 
+global $PHP_SELF; 
+
+$thisfilename=basename(__FILE__); 
+$temp_filename=realpath(__FILE__); 
+if(!$temp_filename) $temp_filename=__FILE__; 
+$osdir=eregi_replace($thisfilename,"",$temp_filename); 
+unset($temp_filename); 
+
+$virdir = eregi_replace($thisfilename,"",$PHP_SELF); 
+
+
+echo "현재 디렉토리의 절대경로 : ".$osdir."<br>"; 
+echo "현재 디렉토리의 상대 경로 주소 : ".$virdir."<br>"; 
+
+?>
